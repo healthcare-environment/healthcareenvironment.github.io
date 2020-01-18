66d4fe31af9dc02d243e6b23a26f5bddf95d700f
github@chrisandrew.tech <github@chrisandrew.tech>
2020-01-18 12:08:39 -0800

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -174,7 +174,67 @@ function slidebar() {
 })(jQuery);
 
 
-function saveFormInput()
+function submitFormToHubSpot(evt)
 {
-    
+    /*d47cfea0-1bc7-4de1-b2cd-68a12bc37a47*/
+
+    var xhr = new XMLHttpRequest();
+    var url =
+      "https://api.hsforms.com/submissions/v3/integration/submit/62515/4072f3c9-130a-4281-8b4f-3c85ab3e1a88?hapikey=d47cfea0-1bc7-4de1-b2cd-68a12bc37a47";
+    var epoch = Date.now()
+
+    // Example request JSON:
+    var data = {
+      submittedAt: epoch,
+      fields: [
+        {
+          name: "email",
+          value: "chrisandrew@outlook.com"
+        },
+        {
+          name: "Chris",
+          value: "Jeff"
+        }
+      ],
+      legalConsentOptions: {
+        // Include this object when GDPR options are enabled
+        consent: {
+          consentToProcess: true,
+          text:
+            "I agree to allow Healthcare Environment to store and process my personal data.",
+          communications: [
+            {
+              value: true,
+              subscriptionTypeId: 999,
+              text:
+                "I agree to receive marketing communications from Healthcare Environment."
+            }
+          ]
+        }
+      }
+    };
+
+    var final_data = JSON.stringify(data);
+    alert(final_data);
+
+    xhr.open("POST", url);
+    // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        alert(xhr.responseText); // Returns a 200 response if the submission is successful.
+      } else if (xhr.readyState == 4 && xhr.status == 400) {
+        alert(xhr.responseText); // Returns a 400 error the submission is rejected.
+      } else if (xhr.readyState == 4 && xhr.status == 403) {
+        alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.
+      } else if (xhr.readyState == 4 && xhr.status == 404) {
+        alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found
+      }
+    };
+
+    // Sends the request
+
+    xhr.send(final_data);    
+
 }
